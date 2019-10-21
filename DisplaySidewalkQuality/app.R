0927072eb7de6e99d8a844acde390f366ec7601b
alankjackson <alan@ajackson.org>
2019-10-20 19:51:31 -0500

    Add more quality flags and colors shrink init weight fiddle line drawing Ignore=TRUE on buttons

--- DisplaySidewalkQuality/app.R
+++ DisplaySidewalkQuality/app.R
@@ -24,7 +24,6 @@ library(leafpop) # for popup on map
 DataLocation <- "https://www.ajackson.org/sidewalks/data/"
 
 #   Tibble database
-
 OldDF <- readRDS(gzcon(url(paste0(DataLocation, "/Photos.rds"))))
 
 #   Sort so that shortest paths plot last to avoid overplotting
@@ -36,12 +35,12 @@ MapCenter <- c(-95.404606 , 29.797131)
 
 #   Colors for various qualities
 colorDF <- data.frame(
-     Quality = c("Good", "Bushes",  "Gap", "Offset", "Shattered", "Obstructed",
-                "Debris/Mud", "Gravel", "No Curb Cut", "Missing"),
-     Colors = c("green", "cyan2", "deepskyblue2", "dodgerblue2", "blue2",
-            "yellow", "goldenrod2", "darkorange2", "magenta2", "red2"),
-     Codes=c("#008000", "#00eeee", "#00b2ee", "#1c86ee", "#0000ee",
-             "#ffff00", "#eeb422", "#ee7600", "#ee00ee", "#ee0000"),
+     Quality = c("Good", "Acceptible", "Bushes",  "Gap", "Offset", "Shattered", "Obstructed",
+                "Debris/Mud", "Gravel", "No Curb Cut", "Missing", "BadPict"),
+     Colors = c("green", "greenyellow", "cyan2", "deepskyblue2", "dodgerblue2", "blue2",
+            "yellow", "goldenrod2", "darkorange2", "magenta2", "red2", "black"),
+     Codes=c("#008000", "#ADFF2F", "#00eeee", "#00b2ee", "#1c86ee", "#0000ee",
+             "#ffff00", "#eeb422", "#ee7600", "#ee00ee", "#ee0000", "#000000"),
      stringsAsFactors = FALSE
 )
 
@@ -53,7 +52,7 @@ OldDF$SourceFile <- paste0(DataLocation, OldDF$SourceFile)
 OldDF <- left_join(OldDF, colorDF, by="Quality")
 
 init_zoom <- 15
-init_weight <- 6
+init_weight <- 4
 
 ##################################################
 # Define UI for displaying and annotating photos
@@ -72,6 +71,7 @@ shinyApp(
                #    Quality
                checkboxGroupInput("quality", label = "Sidewalk qualities to display",
                             choices = list("Good"="Good", 
+                                           "Acceptible"="Acceptible", 
                                            "Bushes"="Bushes", 
                                            "Gap"="Gap",  
                                            "Offset"="Offset", 
@@ -80,7 +80,9 @@ shinyApp(
                                            "Debris/Mud"="Debris/Mud", 
                                            "Gravel"="Gravel", 
                                            "No Curb Cut"="No Curb Cut", 
-                                           "Missing"="Missing"),
+                                           "Missing"="Missing",
+                                           "BadPict"="BadPict"
+                                           ),
                             selected = colorDF$Quality),
                #    Select or unselect all
                     HTML("<hr>"),
@@ -105,7 +107,6 @@ shinyApp(
         ##################################
             output$LocalMap <- renderLeaflet({
                 #   Basemap
-                #leaflet(OldDF) %>% 
                 leaflet() %>% 
                   setView(lng = -95.404606 , lat = 29.797131, zoom = init_zoom ) %>%   
                     addTiles() %>% 
@@ -131,14 +132,14 @@ shinyApp(
                         if (OldDF[i,]$Quality %in% input$quality) {
                             leafletProxy("LocalMap") %>% 
                               addPolylines(
-                                 lat=c(OldDF[i,]$GPSLatitude, OldDF[i,]$EndLat),
-                                 lng=c(OldDF[i,]$GPSLongitude, OldDF[i,]$EndLon),
+                                lat=c(OldDF[i,]$NewLat, OldDF[i,]$EndLat),
+                                lng=c(OldDF[i,]$NewLon, OldDF[i,]$EndLon),
                                  color=OldDF[i,]$Codes,
                                  weight=wgt,
                                  popup = popupImage(OldDF[i,]$SourceFile, src="remote",
                                       height=150,
                                       width=150),
-                                 opacity=1
+                                opacity=1
                               )
                         }
                     }
@@ -159,7 +160,7 @@ shinyApp(
                                          inputId = "quality", 
                                          selected = colorDF$Quality)
             }
-         }, ignoreNULL=FALSE)
+         }, ignoreNULL=TRUE)
 
         #      Unselect    
         observeEvent(input$deselectAll, {
@@ -168,7 +169,7 @@ shinyApp(
                                          inputId = "quality", 
                                          selected = "")
             }
-         }, ignoreNULL=FALSE)
+         }, ignoreNULL=TRUE)
             
     }
 )
